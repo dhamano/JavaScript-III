cb1a3315ee75c239a59374968b4535e0fde41e84
Dustin <clerin+gitkraken@gmail.com>
2019-05-08 12:39:53 -0700

    this.js done

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,45 +1,62 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.
+  For this part of the assignment you will be implementing
+  several constructor functions with their correct inheritance
+  hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor
+  functions: GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up
+  inheriting from Humanoid.  Use the objects at the bottom
+  of the page to test your constructor functions.
   
-  Each constructor function has unique properties and methods that are defined in their block comments below:
+  Each constructor function has unique properties and methods
+  that are defined in their block comments below:
 */
   
 /*
   === GameObject ===
   * createdAt
   * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+  * dimensions (These represent the character's size
+  * in the videogame)
+  * destroy() // prototype method that returns:
+  * `${this.name} was removed from the game.`
 */
 
 /*
   === CharacterStats ===
   * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * takeDamage() // prototype method ->
+  * returns the string'<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
 
 /*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  === Humanoid (Having an appearance or character resembling
+    that of a human.) ===
   * team
   * weapons
   * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
+  * greet() // prototype method ->
+  * returns the string '<object name> offers a greeting in
+  * <object language>.'
+  * should inherit destroy() from GameObject through
+  * CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
+  * Instances of Humanoid should have all of the same
+  * properties as CharacterStats and GameObject.
+  * Instances of CharacterStats should have all of the same
+  * properties as GameObject.
 */
 
-// Test you work by un-commenting these 3 objects and the list of console logs below:
+// Test you work by un-commenting these 3 objects and the
+// list of console logs below:
 
 /*
   const mage = new Humanoid({
